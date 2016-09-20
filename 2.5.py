5d856a2871c70601bc94e0255280febc73962a23
scottgibson <sag122@pitt.edu>
2016-09-19 20:26:23 -0400

    Scott Gibson

---
+++ 2.5.py
@@ -0,0 +1,7 @@
+speed = 90
+
+hours = input('Enter number of hours drive: ')
+
+distance = speed * hours
+
+print( 'Total kilometers driven is' , distance)
---
+++ example.py
@@ -0,0 +1,5 @@
+salary = 2500
+bonus = 1600
+pay = salary + bonus
+#display the pay
+print('Your pay is', pay)
